b8827ffbe0db61788362f3efea5d2b5e84ee4cac
ViniciusMirandaDev <ViniciusMirandaDev>
2020-06-05 14:08:00 -0300

    Projeto Iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula11_Sprint4_Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
